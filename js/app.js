d692c99d33acce73ad8b96d82816bdd8230a2344
MicaGrand <el12del8@hotmail.com>
2023-05-13 03:17:58 -0300

    interaccion con el dom

--- js/app.js
+++ js/app.js
@@ -1,147 +1,232 @@
+
 // Array de productos - Catalogo
+
 const productos = [
     {
         id: 1,
         nombre: "Velador Karen",
+        imagen: "../img/lampara-karen.jpeg",
+        categoria: "iluminacion",
         descripcion: "Base de ceramica blanca. Pantalla de yute. Altura 45 cm",
         precio: 10900
     },
     {
         id: 2,
         nombre: "Velador Tina",
+        imagen: "../img/lampara-tina.jpeg",
+        categoria: "iluminacion",
         descripcion: "Base de cemento. Pantalla de tela. Altura 45 cm",
         precio: 11900
     },
     {
         id: 3,
         nombre: "Puff natural",
+        imagen: "../img/puff.jpeg",
+        categoria: "muebles",
         descripcion: "puff de seagrass. Medidas: 30cm x 45cm",
         precio: 19900
     },
     {
         id: 4,
         nombre: "Espejo circular",
+        imagen: "../img/espejo.jpeg",
+        categoria: "deco",
         descripcion: "Espejo con marco de fibras naturales. Medidas: 45cm",
         precio: 12100
     },
     {
         id: 5,
         nombre: "Banquito Quito",
+        imagen: "../img/banquito.jpeg",
+        categoria: "muebles",
         descripcion: "Banco de madera de álamo. Medidas: ancho 90 cm, alto 65 cm.",
         precio: 15000
     },
     {
         id: 6,
         nombre: "Perchero Pola",
+        imagen: "../img/perchero.jpeg",
+        categoria: "muebles",
         descripcion: "Perchero y zapatero confeccionado en madera de álamo.",
         precio: 25000
     },
     {
         id: 7,
         nombre: "Lámpara hilos",
+        imagen: "../img/lampara-hilos.jpeg",
+        categoria: "iluminacion",
         descripcion: "Lámpara colgante confeccionada en hilos naturales",
         precio: 14000
     },
     {
         id: 8,
         nombre: "Cuadro Home",
+        imagen: "../img/cuadro.jpeg",
+        categoria: "deco",
         descripcion: "Cuadro decorativo. Medidas: 40cm x 60cm",
         precio: 15000
     },
 ];
 
-// Array de productos - Carrito
-const carrito = [];
+let carrito = [];
 
-// Funciones centrales
+// Mostrar productos en catalogo 
+document.addEventListener("DOMContentLoaded", function () {
+    const contenedorTarjetas = document.querySelector("#contenedor-tarjetas");
+    let filaTarjetas = document.createElement("div");
+    filaTarjetas.classList.add("row", "gx-0", "gy-3");
+    contenedorTarjetas.appendChild(filaTarjetas);
 
-// Mostrar catalogo en consola
-function mostrarCatalogo() {
-    console.log("Catálogo:");
-    for (let i = 0; i < productos.length; i++) {
-        const producto = productos[i];
-        console.log(`ID: ${producto.id} - Nombre: ${producto.nombre} - Precio: $${producto.precio}`);
+    function mostrarProductos() {
+        productos.forEach((producto, index) => {
+            const tarjeta = document.createElement("div");
+            tarjeta.classList.add("tarjeta", "col-md-6", "col-lg-4", "col-xl-3", "p-2", "text-center");
+
+            const imagenTarjeta = document.createElement("div");
+            imagenTarjeta.classList.add("tarjeta-img", "img-thumbnail", "position-relative");
+            imagenTarjeta.innerHTML = `
+                <img src="${producto.imagen}" class="w-100">
+            `;
+            tarjeta.appendChild(imagenTarjeta);
+
+            const textoTarjeta = document.createElement("div");
+            textoTarjeta.classList.add("tarjeta-texto", "text-center");
+            textoTarjeta.innerHTML = `
+                <p class="text-capitalize my-1">${producto.nombre}</p>
+                <span class="tarjeta-precio fw-bold">$${producto.precio}</span>
+            `;
+            tarjeta.appendChild(textoTarjeta);
+
+            const botonAgregar = document.createElement("button");
+            botonAgregar.classList.add("tarjeta-boton", "btn", "m-2", "text-dark");
+            botonAgregar.innerText = "Agregar al carrito";
+            botonAgregar.setAttribute("data-producto-id", producto.id);
+            tarjeta.appendChild(botonAgregar);
+
+            botonAgregar.addEventListener("click", () => {
+                agregarCarrito(producto);
+            });
+
+            filaTarjetas.appendChild(tarjeta);
+        });
     }
-}
-mostrarCatalogo();
 
-// Calcular el total del carrito
-function calcularTotalCarrito() {
-    let total = 0;
-    carrito.forEach(function(producto) {
-        total += producto.precio;
-    });
-    return total;
-}
+    mostrarProductos();
 
+    // Carrito
+    // Chequear si hay carrito almacenado en local storage
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-// Eliminar producto del carrito
-function eliminarProductoDeCarrito(idProductoAEliminar) {
-    for (let i = 0; i < carrito.length; i++) {
-        if (carrito[i].id === idProductoAEliminar) {
-            carrito.splice(i, 1);
-            console.log(carrito);
-            alert("Producto eliminado del carrito");
-            return;
-        }
-    }
-    alert("Producto no encontrado en el carrito");
-}
-
-// Vaciar el carrito
-function vaciarCarrito() {
-    carrito.splice(0, carrito.length);
-    console.log(carrito);
-    alert("Carrito vaciado");
-}
-
-
-
-// Agregar productos al carrito
-let continuar = true;
-
-do {
-    const idProducto = parseInt(prompt("Ingresá el ID del producto que querés agregar al carrito (por ej. ingresá 1 si querés agregar un Velador Karen):\n\n" +
-    productos.map(producto => `${producto.id}: ${producto.nombre}`).join("\n") +
-    "\n\nEscribí '0' si terminaste de agregar.\n\n"));
-    
-    const carritoInfo = carrito.map(producto => `${producto.nombre} (cantidad: ${carrito.filter(p => p.id === producto.id).length})`);
-    if (idProducto === 0) {
-        // opciones de como continuar
-        const opciones = parseInt(prompt(`El total de tu carrito es de $ ${calcularTotalCarrito()}.\n\n Productos en el carrito: \n\n ${carritoInfo.join('\n')} \n\n Elegí una opción: \n 1. Seguir agregando productos. \n 2. Continuar al pago \n 3. Eliminar un producto \n 4. Vaciar carrito \n 5. Salir \n\n`));
-        switch (opciones) {
-            case 1:
-                break;
-            case 2:
-                alert("Te estamos redirigiendo al pago...");
-                continuar = false;
-                break;
-            case 3:
-                const idProductoAEliminar = parseInt(prompt("Escribí el id del producto que querés eliminar"));
-                eliminarProductoDeCarrito(idProductoAEliminar);
-                break;
-            case 4:
-                vaciarCarrito();
-                break;
-            case 5:
-                continuar = false;
-                break;
-            default:
-                alert("Opción inválida");
-                break;
-        }
-    } else {
-        // Buscar producto por id en el catalogo
-        const productoEncontrado = productos.find((producto) => producto.id === idProducto);
-
-        // Si se encontró el producto, agregarlo al carrito
-        if (productoEncontrado) {
-            carrito.push(productoEncontrado);
-            alert(`Producto agregado al carrito: ${productoEncontrado.nombre}`);
-            console.log(carrito);
+    function agregarCarrito(producto) {
+        const productoExistente = carrito.find(item => item.id === producto.id);
+        if (productoExistente) {
+            productoExistente.cantidad++;
+            productoExistente.precio += producto.precio;
         } else {
-            alert(`No se encontró el producto con id ${idProducto}`);
+            carrito.push({
+                id: producto.id,
+                nombre: producto.nombre,
+                imagen: producto.imagen,
+                precio: producto.precio * 1,
+                cantidad: 1
+            });
         }
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        console.log(carrito);
     }
-} while (continuar);
+})
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const contenedorCarrito = document.querySelector("#contenedorCarrito");
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    
+    function mostrarCarrito() {
+        contenedorCarrito.innerHTML = "";
+        carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carrito.forEach((producto) => {
+            const productoHTML = document.createElement("div");
+            productoHTML.classList.add("producto", "card", "mb-3", "border", "shadow-sm", "d-flex", "align-items-center");
+            productoHTML.innerHTML = `
+            <img class="card-img-top img-fluid img-thumbnail" src="${producto.imagen}" alt="${producto.nombre}">
+            <div class="card-body text-center">
+                <h3 class="card-title">${producto.nombre}</h3>
+                <p class="card-text">Cantidad: ${producto.cantidad}</p>
+                <p class="card-text">Precio: $${producto.precio}</p>
+            </div>
+        `;
+            contenedorCarrito.appendChild(productoHTML);
+
+            const botonEliminar = document.createElement("button");
+            botonEliminar.classList.add("boton-eliminar", "btn", "m-2", "text-dark");
+            botonEliminar.innerText = "Eliminar";
+            botonEliminar.setAttribute("data-producto-id-eliminar", producto.id);
+
+            productoHTML.appendChild(botonEliminar);
+
+            botonEliminar.addEventListener("click", () => {
+                eliminarProductoDeCarrito(producto.id);
+            });
+        });
+    }
+
+    // Eliminar productos
+
+    function eliminarProductoDeCarrito(idProducto) {
+        carrito = carrito.filter((producto) => producto.id !== idProducto);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        actualizarTotal();
+        mostrarCarrito();
+    }
+    
+    mostrarCarrito();
+    
+    
+
+
+ // Calcular total
+function calcularTotal() {
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    return carrito.reduce((total, producto) => total + producto.precio, 0);
+}
+
+let total = calcularTotal();
+console.log(total);
+const totalCarritoHTML = document.querySelector("#totalCarrito");
+totalCarritoHTML.classList.add("total-carrito");
+totalCarritoHTML.innerText = `El total de tu compra es: $${total}`;
+
+const botonVaciar = document.createElement("button");
+botonVaciar.innerText = "Vaciar carrito";
+totalCarritoHTML.appendChild(botonVaciar);
+botonVaciar.addEventListener("click", () => {
+    vaciarCarrito();
+});
+
+// Actualizar total
+
+function actualizarTotal() {
+    let totalActualizado = 0;
+    carrito.forEach((producto) => {
+        totalActualizado += producto.precio * producto.cantidad;
+    });
+    const totalActualizadoHTML = document.querySelector("#totalCarrito");
+    totalActualizadoHTML.innerText = `El total de tu compra es: $${totalActualizado}`;
+}
+
+
+function vaciarCarrito() {
+    carrito = [];
+    localStorage.removeItem("carrito");
+    actualizarTotal();
+    if (total === 0) {
+        totalCarritoHTML.innerHTML = `
+            <div class="card m-auto">
+                <div class="card-body text-center">
+                    <h3 class="card-title">Carrito vacío</h3>
+                    <p class="card-text">Agrega productos a tu carrito para comenzar a comprar</p>
+                </div>
+            </div>`;
+    }
+}}
+)
